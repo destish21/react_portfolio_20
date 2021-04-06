5b5375dec89cd6ec2a7a2facf1fff008e9b7ed85
desta mulu <destish21@yahoo.com>
2021-04-06 14:41:02 -0500

    Updated Resum link

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -7,29 +7,29 @@ const Navbar = () => {
         <div id="container">
             <nav className="navbar navbar-expand-lg navbar-light" >
                 <h1>Desta Mulualem</h1>
-                <button className="navbar-toggler" type="button"   data-toggle="collapse" data-target="#navbarSupportedContent"
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
+                        <li className="nav-item active">
                             <Link to="/" className="nav-link" href="/">Home</Link>
-                         </li>
+                        </li>
                         <li className="nav-item active">
                             <Link to="/contact" className="nav-link" href="/contact">Contact</Link>
                         </li>
                         <li className="nav-item active">
                             <Link to="/portfolio" className="nav-link" href="/portfolio">Portfolio <span className="sr-only">(current)</span></Link>
-                          </li>
-                          
-                         <li className="nav-item active">
+                        </li>
+
+                        <li className="nav-item active">
                             <Link to="/about" className="nav-link" href="/about">About</Link>
-                         </li>
+                        </li>
                     </ul>
-             </div>
-          </nav>      
+                </div>
+            </nav>
         </div>
     )
 }
